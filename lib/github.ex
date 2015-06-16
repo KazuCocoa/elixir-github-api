305c1a762d2756236ff11cd2a2f9fcbdf7b63c0f
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-06-17 07:57:09 +0900

    add api of milestones

--- lib/github.ex
+++ lib/github.ex
@@ -3,7 +3,7 @@ defmodule Github do
 
   defmodule Config do
     defstruct api: "https://api.github.com",
-              token: "set token"
+              token: "your token"
   end
 
   defmodule GithubResponseError do
@@ -21,33 +21,10 @@ defmodule Github do
     end
   end
 
-  @doc """
-   [avatar_url: "https://avatars.githubusercontent.com/u/5511591?v=3", bio: nil,
-   blog: "http://kazucocoa.wordpress.com", collaborators: 2,
-   company: "Cookpad(see: kazu-matsu)", created_at: "2013-09-22T07:56:17Z",
-   disk_usage: 7351, email: "matsuo@cookpad.com",
-   events_url: "https://api.github.com/users/KazuCocoa/events{/privacy}",
-   followers: 6,
-   followers_url: "https://api.github.com/users/KazuCocoa/followers",
-   following: 3,
-   following_url: "https://api.github.com/users/KazuCocoa/following{/other_user}",
-   gists_url: "https://api.github.com/users/KazuCocoa/gists{/gist_id}",
-   gravatar_id: "", hireable: false, html_url: "https://github.com/KazuCocoa",
-   id: 5511591, location: "", login: "KazuCocoa", name: "Kazuaki MATSUO",
-   organizations_url: "https://api.github.com/users/KazuCocoa/orgs",
-   owned_private_repos: 3,
-   plan: %{"collaborators" => 0, "name" => "micro", "private_repos" => 5,
-     "space" => 976562499}, private_gists: 23, public_gists: 1, public_repos: 48,
-   received_events_url: "https://api.github.com/users/KazuCocoa/received_events",
-   repos_url: "https://api.github.com/users/KazuCocoa/repos", site_admin: false,
-   starred_url: "https://api.github.com/users/KazuCocoa/starred{/owner}{/repo}",
-   subscriptions_url: "https://api.github.com/users/KazuCocoa/subscriptions",
-   total_private_repos: 3, type: "User", updated_at: "2015-06-15T08:50:23Z",
-   url: "https://api.github.com/users/KazuCocoa"]
-  """
   def get_user_info do
     url = "/user"
     headers = [{"Authorization", " token #{%Github.Config{}.token}"}]
+
     case Github.get(url, headers) do
       {:ok, response} ->
         response.body
@@ -56,7 +33,17 @@ defmodule Github do
     end
   end
 
+  def get_milestones(repo, params \\ %{}) do
+    url = "/repos/#{repo}/milestones"
+    headers = [{"Authorization", " token #{%Github.Config{}.token}"}]
 
+    case Github.get(url, headers, params: params) do
+      {:ok, response} ->
+        response.body
+      {:error, reason} ->
+        raise [:error, reason]
+    end
+  end
 
   defp process_url(url) do
     %Github.Config{}.api <> url
@@ -66,7 +53,7 @@ defmodule Github do
   defp process_response_body(body) do
     body
     |> Poison.decode!
-    |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
+  #  |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
   end
 
 end
--- test/github_test.exs
+++ test/github_test.exs
@@ -1,7 +1,22 @@
 defmodule GithubTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "get_top" do
+    assert Github.get_top != empty?
+  end
+
+  test "get_user_info" do
+    assert Github.get_user_info != empty?
+  end
+
+  test "get_milestones" do
+    params = %{state: "closed"}
+    repo = "KazuCocoa/tagTestRepository"
+
+    assert Github.get_milestones(repo, params) != empty?
+  end
+
+  defp empty? do
+    nil || ""
   end
 end
